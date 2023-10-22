57a16817e73f0aedf22a2f67e1a04c5c779f5150
Roshan Tajpuriya <37438262+roshantaj25@users.noreply.github.com>
2023-10-22 09:50:13 +0530

    Images are into local

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const images = [
-    "https://images.unsplash.com/photo-1697469994783-b12bbd9c4cff?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1697451964285-efa976c9178b?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1697230188928-fb36c4964bf7?auto=format&fit=crop&q=80&w=3387&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1685350767266-a0fc514fb41e?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1697581886387-a8b3af94ef0b?auto=format&fit=crop&q=80&w=3451&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1697487957555-5f8df6a965f6?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1697638332749-da3ba4046082?auto=format&fit=crop&q=80&w=3542&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1694250990115-ca7d9d991b24?auto=format&fit=crop&q=80&w=3395&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1697386576502-812fce23b209?auto=format&fit=crop&q=80&w=3328&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1697716080597-be01c4946985?auto=format&fit=crop&q=80&w=3475&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1697554521121-3a3cda83105a?auto=format&fit=crop&q=80&w=3264&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1697485538022-766751333c4f?auto=format&fit=crop&q=80&w=3432&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1619807811096-7d0584eedecf?auto=format&fit=crop&q=80&w=3008&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    "images/photo-1697469994783-b12bbd9c4cff.avif",
+    "images/photo-1697451964285-efa976c9178b.avif",
+    "images/photo-1697230188928-fb36c4964bf7.avif",
+    "images/photo-1685350767266-a0fc514fb41e.avif",
+    "images/photo-1697581886387-a8b3af94ef0b.avif",
+    "images/photo-1697487957555-5f8df6a965f6.avif",
+    "images/photo-1697638332749-da3ba4046082.avif",
+    "images/photo-1694250990115-ca7d9d991b24.avif",
+    "images/photo-1697386576502-812fce23b209.avif",
+    "images/photo-1697716080597-be01c4946985.avif",
+    "images/photo-1697554521121-3a3cda83105a.avif",
+    "images/photo-1697485538022-766751333c4f.avif",
+    "images/photo-1619807811096-7d0584eedecf.avif"
 ]
 const imageList = $('.image-slider-container-wrapper-right')
 let interval = ''
